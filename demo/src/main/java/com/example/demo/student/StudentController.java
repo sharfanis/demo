ffd485de6ee8e3192c6eaa790eb802616f6c3c5d
shabih <shabih.sharfani@gmail.com>
2023-09-12 15:09:00 -0500

    Added decorators

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -3,7 +3,6 @@ package com.example.demo.student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
