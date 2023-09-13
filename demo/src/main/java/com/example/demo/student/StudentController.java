f02d79363041b832e50ed11ab006e99d5f63f197
shabih <shabih.sharfani@gmail.com>
2023-09-13 12:30:07 -0500

    Added config settings for the DB connection

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+
 @RestController
 @RequestMapping(path="api/v1/students")
 public class StudentController {
